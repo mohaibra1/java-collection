83873420e04cda7dca885c36a1e5a2fa586cea99
mohaibra <mimohadinho@gmail.com>
2023-08-16 23:50:39 +0200

    learning used of collections

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Java-Collection
+
+ArrayList -> Can contain Duplicates; Maintains the insertion order; Can randomly accessed
+LinkedList -> Linked on both ends; Cant contain duplicates; Maninpulation is fast since no shifting required
+Vector -> Similar to ArrayList but synchronized and has many methods than ArrayList
+Stack -> Lifo(last in fast out)
+
+PriorityQueue -> Fifo; Doesnt allow null elements
+ArrayDueue -> Double end operation; Faster than ArrayList and Stack
+
+HashSet -> contains unique elements; Allows at most one null value; Hashing is used to store the elements
+TreeSet -> Unique elements; Sorted in ascending order; TreeSet is quite fast
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,46 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.ListIterator;
+import java.util.Set;
+import java.util.Stack;
+import java.util.TreeSet;
+
 public class Main{
 
 
     public static void main(String[] args) {
-        System.out.println("Hallo World!");
+
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        
+        Set<String> set = new TreeSet<>();    
+       
+        String[] str = {"Love", "Joke", "Hence", "You", "You","Hence","Joke"};
+         ArrayList<String> arr = new ArrayList<>(Arrays.asList(str));
+
+         arr.stream().map(s -> set.add(s))
+         .forEach(System.out::println);
+
+         set.stream().map(s -> s + " ")
+         .forEach(System.out::print);
+         System.out.println();
+
+        LinkedHashSet<String> linkedHashSet = new LinkedHashSet<>();
+
+        arr.stream().map(s -> linkedHashSet.add(s))
+        .forEach(System.out::print);
+        System.out.println();
+        linkedHashSet.stream().map(s -> s + " ")
+        .forEach(System.out::print);
+        System.out.println();
+
+
+        
+        System.out.println(stack.pop() + " " + stack.pop() + " " + stack.peek());
     }
 }
